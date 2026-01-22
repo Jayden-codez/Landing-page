d1c5a88981dcaf703818ec71de0cc546f003c7a9
Jayden <jayden98j@gmail.com>
2026-01-22 14:24:26 -0300

    Added Bitcoin Calculator function

---
+++ BTCUSD/script.js
@@ -0,0 +1,20 @@
+ function result() {
+    let contratos = document.getElementById("contratos").value;
+    let pontos = document.getElementById("pontos").value;
+    let resultado = pontos * contratos;
+    document.getElementById("result").textContent = `$${resultado}`;
+}
+
+ function result2() {
+    let pontos = document.getElementById("pontos2").value;
+    let stopLoss = document.getElementById("stopLoss").value;
+    let resultado2 = stopLoss / pontos;
+    document.getElementById("result2").textContent = resultado2;
+}
+
+function result3() {
+    let contratos = document.getElementById("contratos2").value;
+    let stopLoss = document.getElementById("stopLoss2").value;
+    let resultado3 = stopLoss / contratos;
+    document.getElementById("result3").textContent = resultado3;
+}
