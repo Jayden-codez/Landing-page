2b0325f6f0569a5874bb6f758517350a8649bf96
Jayden <jayden98j@gmail.com>
2026-01-22 13:22:23 -0300

    Simplified the formula

--- XAUUSD/script.js
+++ XAUUSD/script.js
@@ -1,14 +1,14 @@
  function result() {
     let contratos = document.getElementById("contratos").value;
     let pips = document.getElementById("pontos").value;
-    let resultado = (contratos * 100) * pips * 0.01;
+    let resultado = contratos * pips;
     document.getElementById("result").textContent = `$${resultado}`;
 }
 
  function result2() {
     let pips = document.getElementById("pontos2").value;
     let stopLoss = document.getElementById("stopLoss").value;
-    let resultado2 = stopLoss / pips / 100;
+    let resultado2 = stopLoss / pips;
     document.getElementById("result2").textContent = resultado2;
 }
 
@@ -16,7 +16,8 @@ function result3() {
     let contratos = document.getElementById("contratos2").value;
     let stopLoss = document.getElementById("stopLoss2").value;
     let resultado3 = stopLoss / contratos;
-    document.getElementById("result3").textContent = resultado3;
+    document.getElementById("result3").textContent = Math.round(resultado3);
 
 }
+
 
