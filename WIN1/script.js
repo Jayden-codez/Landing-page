ee9a5709e8a5b2f15f640f788294a364b42a2f01
Jayden <jayden98j@gmail.com>
2026-01-22 15:53:40 -0300

    rounding numbers

--- WIN1/script.js
+++ WIN1/script.js
@@ -2,19 +2,20 @@
     let contratos = document.getElementById("contratos").value;
     let pontos = document.getElementById("pontos").value;
     let resultado = pontos * contratos * 10;
-    document.getElementById("result").textContent = `R$${resultado}`;
+    document.getElementById("result").textContent = `R$${Math.round(resultado * 100) / 100}`;
 }
 
  function result2() {
     let pontos = document.getElementById("pontos2").value;
     let stopLoss = document.getElementById("stopLoss").value;
     let resultado2 = (stopLoss / pontos) / 10;
-    document.getElementById("result2").textContent = resultado2;
+    document.getElementById("result2").textContent = Math.round(resultado2 * 100) / 100;
 }
 
 function result3() {
     let contratos = document.getElementById("contratos2").value;
     let stopLoss = document.getElementById("stopLoss2").value;
     let resultado3 = (stopLoss / contratos) / 10;
-    document.getElementById("result3").textContent = resultado3;
+    document.getElementById("result3").textContent = Math.round(resultado3 * 100) / 100;
+
 }
